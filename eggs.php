6e7e43a41cdcc6ad68416bdb9bdef15c52aa9b2a
antonioortegajr <antonioortegajr@gmail.com>
2015-01-19 20:06:51 -0800

    Update eggs.php

--- eggs.php
+++ eggs.php
@@ -5,9 +5,63 @@ If (){
 
 switch(){
 
-case:
+//star trek section 
 
+case "the needs of the many":
+"{outway the needs of the few}";
 break;
+
+case "spock":
+"{live long and prosper";
+break;
+
+case "red shirt":
+"{he's dead Jim}";
+break;
+
+case "":
+"";
+break;
+
+case "":
+"";
+break;
+
+case "":
+"";
+break;
+
+case "":
+"";
+break;
+
+case "":
+"";
+break;
+
+case "":
+"";
+break;
+
+case "":
+"";
+break;
+
+case "":
+"";
+break;
+
+case "":
+"";
+break;
+
+// section 
+
+case "":
+"";
+break;
+
+
 
 }
 
