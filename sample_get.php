6dc3630b3aeda510b00b0881a1ab31e2c2eb91e4
antonioortegajr <antonioortegajr@gmail.com>
2015-01-19 21:55:55 -0800

    Update sample_get.php
    changed explode  a little and added notes

--- sample_get.php
+++ sample_get.php
@@ -35,13 +35,7 @@ curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-/*
-if ($method != 'GET'){
-curl_setopt($handle, CURLOPT_CUSTOMREQUEST, $method);
-// send the data
-curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
-}
-*/
+
 // exec the cURL request and returned information. Store the returned HTTP code in $code for later reference
 $response = curl_exec($handle);
 $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
@@ -51,9 +45,9 @@ $total_download_size = curl_getinfo($handle, CURLINFO_SIZE_DOWNLOAD);
 $connect_time = curl_getinfo($handle, CURLINFO_CONNECT_TIME);
 //returns the response headers
 curl_setopt($handle, CURLOPT_HEADER, 1);
-
-
 //Get the response headers and the body, use explode() to seperate into two at the first 'keep-alive' which is always at the end of the return headers
+
+
 $response = curl_exec($handle);
 $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 $b_response = (explode('keep-alive', $response, 2));
@@ -109,8 +103,7 @@ switch ($code) {
   break;
 }
 
-//create jira ticketing link. You can remove this area if not needed.
-
+//create jira link
 //set live or sandbox for jira link
 if ($url_live == 0){
   $j_environment = '10300';
@@ -139,11 +132,12 @@ $j_summary = 'API issue';
 $j_url = $url;
 $j_url_end_point_amp = str_replace("&","%26",$j_url);
 $j_url_end_point = str_replace("+","%2B",$j_url_end_point_amp);
-$internal_jira_link = 'YourlinktoJira';
 $j_description  = 'Summary:%0A%0AExpected+Behavior:%0A%0AActual+Behavior:%0AReturned+error+code:+' . $code . '%0A%0A+%0ASteps+to+replicate+this+issue:%0A+*+API+GET+call+using+the+following+endpoint:+' . $j_url_end_point . '+%0A+*+API+key:+'. $api_key . '+%0A+*+output+requested:+' . $out . '+%0A+*+version+requested:+' . $version;
-$issue_link = 'http://' . $internal_jira_link . '/jira/secure/CreateIssueDetails!init.jspa?pid=10300&issuetype=1&priority=3&customfield_11400=' . $j_environment . '&customfield_11603=' . $j_user_level . '&components=10503&summary=' . $j_summary . '&description=' . $j_description;
+$issue_link = 'http://jira.idx.local/jira/secure/CreateIssueDetails!init.jspa?pid=10300&issuetype=1&priority=3&customfield_11400=' . $j_environment . '&customfield_11603=' . $j_user_level . '&components=10503&summary=' . $j_summary . '&description=' . $j_description;
 echo '</b><br><h3 class="flag"><div id="j_link"><a href="' . $issue_link . '" target="_blank">Create Issue in jira</a></div></h3>';
 
+
+//use json_decode() to validate the response
 if ($out == 'json'){
 
 $json_decode_check = $e . $b_response[1];
@@ -168,15 +162,18 @@ switch (json_last_error()) {
   $check_json =  ' - <div class="red inline">Malformed UTF-8 characters, possibly incorrectly encoded</div>';
   break;
   default:
-  $check_json =  ' - <div class="red inline">Unknown error</div>';
+  $check_json =  ' - <div class="red inline">Unknown json error</div>';
   break;
 }
 
-}
+};
 
-
+$api_response_body = $b_response[1];
 
 //results of tests
+
+echo $bracket_in_response;
+echo '</b><br>';
 echo '<br><br>API key used: <b>';
   echo $api_key;
   echo '</b><br>';
@@ -190,7 +187,7 @@ echo '<br><br>API key used: <b>';
   echo '<br><br><b>Returned headers: </b>';
   echo var_dump($b_response[0]) . 'keep-alive';
   echo '<br><hr><b>API Response Body: ' . $check_json . '</b><br>';
-  echo $e . $b_response[1];
+  echo $api_response_body;
 
 
 
