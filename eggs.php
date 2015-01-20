b2ed202cdc49950fcbbde3cc21e9e51b2b47c4c7
antonioortegajr <antonioortegajr@gmail.com>
2015-01-20 00:19:08 -0800

    Update eggs.php
    added values and response bodies for string entered in API key field.

--- eggs.php
+++ eggs.php
@@ -1,70 +1,120 @@
+<?php
 // Easter eggs
 
 
-If (){
+switch($api_key){
 
-switch(){
+  //star trek section
 
-//star trek section 
+  case "the needs of the many":
+  $api_response_body = "{\"outway\":\"the needs of the few\"}";
+  break;
+  case "Spock":
+  $api_response_body = "{\"live long\":\"prosper\"}";
+  break;
+  case "red shirt":
+  $api_response_body = "{\"he's\":\"dead Jim\"}";
+  break;
+  case "Chekov":
+  $api_response_body = "{\"wessels\":\"nuclear\"}";
+  break;
+  case "Sulu":
+  $api_response_body = "{\"Oh\":\"my\"}";
+  break;
 
-case "the needs of the many":
-"{outway the needs of the few}";
+  //  Marvel section
+  case "Spider-Man":
+  $api_response_body = "{\"does\":\"whatever a spider can\"}";
+  break;
+  case "Captain America":
+  $api_response_body = "{\"stars\":\"stripes\"}";
+  break;
+  case "Hulk":
+  $api_response_body = "<div style=\"font-size: 350%;\">{\"SMASH\":\"SMASH\"}</div>";
+  break;
+  case "Wolverine":
+  $api_response_body = "{\"Snikt!\":\"Snikt!\"}";
+  break;
+
+
+//DC section
+  case "Batman":
+  $api_response_body = "{\"na\":\"na\",\"na\":\"na\",\"na\":\"na\",\"na\":\"na\"}";
+  break;
+  case "Aquaman":
+  $api_response_body = "{\"under\":\"the sea\"}";
+  break;
+  case "Superman":
+  $api_response_body = "{\"up up\":\"away\"}";
+  break;
+
+//firefly
+case "Jayne":
+$api_response_body = "{\"Hero of Canton\":\"man they call Jayne\"}";
+break;
+case "Wash":
+$api_response_body = "{\"leaf on the wind\":\"soar\"}";
+break;
+case "Mal":
+$api_response_body = "{\"aim\":\"misbehave\"}";
 break;
 
-case "spock":
-"{live long and prosper";
+//star wars section
+case "Yoda":
+$api_response_body = "{\"Do\":\"Do not\"}";
+break;
+case "Han":
+$api_response_body = "{\"shoots\":\"first\"}";
+break;
+case "Leia":
+$api_response_body = "{\"Love\":\"you\",\"I\":\"know\"}";
+break;
+case "death star":
+$api_response_body = "{\"moon\":\"no\"}";
+break;
+case "R2D2":
+$api_response_body = "{\"bleep\":\"bloop\",\"blop\":\"boop\"}";
+break;
+case "Lando":
+$api_response_body = "{\"slimy\":\"double-crossing\",\"no-good\":\"swindler\"}";
+break;
+case "Luke":
+$api_response_body = "{\"father\":\"Vader\",\"possible\":\"no\"}";
+break;
+case "Akbar":
+$api_response_body = "{\"trap\":\"y\"}";
+break;
+case "Jabba":
+$api_response_body = "{\"Chool kanya wee\":\"shaja mee-choo\"}";
 break;
 
-case "red shirt":
-"{he's dead Jim}";
+
+//simpsons section
+case "Burns":
+$api_response_body = "{\"Release\":\"hounds\"}";
+break;
+case "Dr. Nick":
+$api_response_body = "{\"Hi\":\"everybody\"}";
+break;
+case "Nelson":
+$api_response_body = "{\"Haw\":\"Ha\"}";
 break;
 
-case "":
-"";
-break;
-
-case "":
-"";
-break;
-
-case "":
-"";
-break;
-
-case "":
-"";
-break;
-
-case "":
-"";
-break;
-
-case "":
-"";
-break;
-
-case "":
-"";
-break;
-
-case "":
-"";
-break;
-
-case "":
-"";
-break;
-
-// section 
-
-case "":
-"";
-break;
-
+//random
+  case "test":
+  $api_response_body = "{\"test????? booooring\":\"try some comic book or sci-fi chracters or something fun\"}";
+  break;
+  case "Captain Planet":
+  $api_response_body = "{\"our\":\"hero\",\"polution\":\"0\"}";
+  break;
+  case "witch doctor":
+  $api_response_body = "{\"oo\":\"ee\",\"oo\":\"ah ah\",\"ting\":\"tang\",\"Walla Walla\":\"bing bang\"}";
+  break;
+  case "":
+  $code = "406";
+  break;
 
 
 }
 
-
-
-}
+?>
